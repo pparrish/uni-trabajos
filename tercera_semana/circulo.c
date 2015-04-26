d5c2f705868c1daa4b6363a3b5e484932a4fd69d
parrish <paarriisshh@yahoo.com.mx>
2015-04-26 02:16:57 -0500

    primera version de circulo.c

---
+++ tercera_semana/circulo.c
@@ -0,0 +1,36 @@
+/***
+ *       _____                  __                  ______                _          ______    __                    ______     _      _   _         
+ *      / ___/____ _____  _____/ /_  ___  ____     / ____/___ ___________(_)___ _   / ____/___/ /___ _____ ______   / ____/____(_)____/ /_(_)___ _____ 
+ *      \__ \/ __ `/ __ \/ ___/ __ \/ _ \/_  /    / / __/ __ `/ ___/ ___/ / __ `/  / __/ / __  / __ `/ __ `/ ___/  / /   / ___/ / ___/ __/ / __ `/ __ \
+ *     ___/ / /_/ / / / / /__/ / / /  __/ / /_   / /_/ / /_/ / /  / /__/ / /_/ /  / /___/ /_/ / /_/ / /_/ / /     / /___/ /  / (__  ) /_/ / /_/ / / / /
+ *    /____/\__,_/_/ /_/\___/_/ /_/\___/ /___/   \____/\__,_/_/   \___/_/\__,_/  /_____/\__,_/\__, /\__,_/_/      \____/_/  /_/____/\__/_/\__,_/_/ /_/ 
+ *     
+ *  circulo.c
+ *  
+ *                                                                                                      
+ */
+ /*Anexar conio solo en windows*/
+#ifdef _WIN32
+#include <conio.h>
+#endif
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+
+  float PI = 3.14159;
+  float radio;
+
+  printf("Ingrese el radio de un circulo\n");
+  scanf("%f", &radio);
+
+  printf("El diametro del circulo es: %f\nEl circunferencia del circulo es: %f\nEl area del circulo es: %f\n", radio*2, PI*radio*2 , PI * radio * radio );
+  
+  #ifdef _WIN32
+  getch();
+  #endif
+
+  return;
+
+}
