a5f1f14348d2d8b8e6d954633146edb264e425ef
parrish <paarriisshh@yahoo.com.mx>
2015-04-26 00:07:04 -0500

    Segunda version de adivinaHora.c XD no entendi bien el problema jajaja

--- tercera_semana/adivinarHora.c
+++ tercera_semana/adivinarHora.c
@@ -15,44 +15,18 @@
 #endif
 
 #include <stdio.h>
-#include <time.h>
 
 void main(){
- 
-  /*La fecha y hora del sistema se obtienen primero incluyendo la libreria time.h
-  Esta libreria ofrese un tipo de dato llamada time_t y otra
-  estructura que se llama tm
-  Nesesitamos primero la funcion time, pasando como parametro la direccion de memoria
-  de un time_t (&) para que este recoja la hora el sistema en el dato time
-
-  por ultimo nesesitamos integrar time_t en una estructura con la que hay que trabajar 
-  para eso usamos un puntero a una estructura tm y la funcion localtime(&time_t) siendo time_t
-  la variable que capturamos antes, con esta estructura podremos trabajar mediante
-  tm->tm_hour
-  tm->tm_min
-  tm->tm_sec
-
-  siendo tm el nombre de la estructura que generamos
-  
-  estos serian las sentencias que usaremos
-  time_t fecha_sistema;
-  time(&fecha_sistema);
-  struct tm *hora = localtime(&fecha_sistema);
-  
-  */
-  time_t fecha_sistema;
   int hour,
       min,
       sec;
   /*Entrada de datos*/
+  printf("teclee  la hora como h:m:s \n");
   scanf("%d:%d:%d", &hour, &min, &sec);
 
-  /*obtenemos la hora del sistema*/
-  time(&fecha_sistema);
-  struct tm *ahora = localtime(&fecha_sistema);
-
+  
   /*salida de datos*/
-  if( (ahora->tm_sec == sec) && (ahora->tm_min == min) && (ahora->tm_sec <= sec) )
+  if(hour <= 24 && hour >= 0 &&  min <= 60 && min >= 0 && sec <= 60 && sec >= 0 )
     printf("hora correcta\n");
   else
     printf("hora incorrecta\n");
