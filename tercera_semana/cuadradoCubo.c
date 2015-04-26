0b9d5955bf3151badb67a7ce35aec848bc6674b1
parrish <paarriisshh@yahoo.com.mx>
2015-04-26 02:37:27 -0500

    primera version de cuadradoCubo

---
+++ tercera_semana/cuadradoCubo.c
@@ -0,0 +1,38 @@
+/***
+ *       _____                  __                  ______                _          ______    __                    ______     _      _   _         
+ *      / ___/____ _____  _____/ /_  ___  ____     / ____/___ ___________(_)___ _   / ____/___/ /___ _____ ______   / ____/____(_)____/ /_(_)___ _____ 
+ *      \__ \/ __ `/ __ \/ ___/ __ \/ _ \/_  /    / / __/ __ `/ ___/ ___/ / __ `/  / __/ / __  / __ `/ __ `/ ___/  / /   / ___/ / ___/ __/ / __ `/ __ \
+ *     ___/ / /_/ / / / / /__/ / / /  __/ / /_   / /_/ / /_/ / /  / /__/ / /_/ /  / /___/ /_/ / /_/ / /_/ / /     / /___/ /  / (__  ) /_/ / /_/ / / / /
+ *    /____/\__,_/_/ /_/\___/_/ /_/\___/ /___/   \____/\__,_/_/   \___/_/\__,_/  /_____/\__,_/\__, /\__,_/_/      \____/_/  /_/____/\__/_/\__,_/_/ /_/ 
+ *     
+ *  cuadradoCubo.c
+ *  
+ *  Regresa el cuadrado y el cubo de un numero dado
+ *                                                                                                      
+ */
+ /*Anexar conio solo en windows*/
+#ifdef _WIN32
+#include <conio.h>
+#endif
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+
+  float num;
+
+  printf("Ingrese un numero\n");
+  scanf("%f", &num);
+
+  /*El usar %e saldra el numero en cuestion en notacion cientifica*/
+  printf("El cuadrado del numero es : %f\n El cubo del numero es: %e\n", num*num, num*num*num );
+
+
+  #ifdef _WIN32
+  getch();
+  #endif
+
+  return;
+
+}
