cfb7940bdee07de259710b37cce7e0cb82afb9ce
parrish <paarriisshh@yahoo.com.mx>
2015-05-07 00:50:34 -0500

    Agregado mayor.c

---
+++ cuarta_semana/mayor.c
@@ -0,0 +1,45 @@
+/***
+ *       _____                  __                  ______                _          ______    __                    ______     _      _   _         
+ *      / ___/____ _____  _____/ /_  ___  ____     / ____/___ ___________(_)___ _   / ____/___/ /___ _____ ______   / ____/____(_)____/ /_(_)___ _____ 
+ *      \__ \/ __ `/ __ \/ ___/ __ \/ _ \/_  /    / / __/ __ `/ ___/ ___/ / __ `/  / __/ / __  / __ `/ __ `/ ___/  / /   / ___/ / ___/ __/ / __ `/ __ \
+ *     ___/ / /_/ / / / / /__/ / / /  __/ / /_   / /_/ / /_/ / /  / /__/ / /_/ /  / /___/ /_/ / /_/ / /_/ / /     / /___/ /  / (__  ) /_/ / /_/ / / / /
+ *    /____/\__,_/_/ /_/\___/_/ /_/\___/ /___/   \____/\__,_/_/   \___/_/\__,_/  /_____/\__,_/\__, /\__,_/_/      \____/_/  /_/____/\__/_/\__,_/_/ /_/ 
+ *     
+ *  mayor.c
+ *  
+ *  imprime el mayor de 3 numeros
+ *                                                                                                      
+ */
+ /*Anexar conio solo en windows*/
+#ifdef _WIN32
+#include <conio.h>
+#endif
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+  int a, b, c;
+
+  printf("Ingresa 3 numeros separados por espacio\n");
+  scanf("%d %d %d", &a, &b, &c);
+
+  if(a > b && b<c){
+    /*es a*/
+    printf("%d es el mas grande\n", &a );
+  }else if (b > c){
+    /*es b*/
+    printf("%d es el mas grande\n", &b );
+  }else{
+    /*es c*/
+    printf("%d es el mas grande\n", &c );
+
+  }
+
+  #ifdef _WIN32
+  getch();
+  #endif
+
+  return;
+
+}
